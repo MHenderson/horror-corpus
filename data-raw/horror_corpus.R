dbd989bde249c34d140b51c0c5736da83351c81a
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-05-24 17:57:05 +0000

    moar datas

--- data-raw/horror_corpus.R
+++ data-raw/horror_corpus.R
@@ -1,5 +1,9 @@
 # https://github.com/ropensci/gutenbergr/issues/28
 # https://www.datasciencemadesimple.com/remove-duplicate-rows-r-using-dplyr-distinct-function/
+library(dplyr)
+library(gutenbergr)
+library(here)
+library(stringr)
 
 alt_mirror <- "http://www.mirrorservice.org/sites/ftp.ibiblio.org/pub/docs/books/gutenberg/"
 
@@ -8,12 +12,15 @@ horror_bookshelf_metadata <- gutenberg_metadata %>%
   filter(language == "en") %>%
   distinct(title, author, .keep_all = TRUE)
 
-horror_book_ids <- head(horror_bookshelf_metadata$gutenberg_id)
+horror_book_ids <- horror_bookshelf_metadata$gutenberg_id
 
-horror_books <- gutenberg_download(horror_book_ids, mirror = alt_mirror) %>%
-  group_by(gutenberg_id) %>% summarise(text = paste(text, collapse = " "))
+horror_books <- horror_book_ids %>%
+  gutenberg_download(mirror = alt_mirror) %>%
+  group_by(gutenberg_id) %>%
+  summarise(text = paste(text, collapse = " "))
 
-horror_books_w_metadata <- left_join(horror_books, horror_bookshelf_metadata) %>%
+horror_books_w_metadata <- horror_books %>%
+  left_join(horror_bookshelf_metadata) %>%
   select(title, text, author)
 
 saveRDS(horror_books_w_metadata, file = "data/horror.rds")
